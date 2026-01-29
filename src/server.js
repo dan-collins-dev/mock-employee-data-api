301125dfcb10ae83c13e038b8ffd2b4f50e5b091
Dan Collins <dan.collins.dev@gmail.com>
2026-01-29 16:53:59 -0500

    initial commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const employeesPath = path.join(__dirname, "../mock-employee-data.json");
+const employees = JSON.parse(fs.readFileSync(employeesPath, "utf-8"));
+
+const app = express();
+
+app.get("/employees", (req, res) => {
+    res.status(200).json(employees);
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
